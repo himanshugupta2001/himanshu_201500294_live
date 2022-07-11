c927eed251faf2dd8eaaccf377a6295f1181d9bb
Himanshu Gupta <himg903@gmail.com>
2022-07-11 20:31:54 +0530

    temp

---
+++ script.js
@@ -0,0 +1,22 @@
+function myFunction()
+{
+    
+    document.getElementById("1").innerHTML = "Himanshu Gupta";
+    document.getElementById("2").innerHTML = "201500294";
+
+    document.getElementById("3").innerHTML =  "GLA University, Mathura";
+
+    document.getElementById("4").innerHTML = "11-07-2022  ";
+}
+
+
+
+
+
+
+
+     
+        
+
+        
+       
